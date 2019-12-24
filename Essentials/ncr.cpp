cc6bb0bc1712d0124cd9aec0eae9f05483c03773
tejas01101001 <45798981+tejas01101001@users.noreply.github.com>
2019-12-24 20:00:55 +0530

    Add files via upload

---
+++ Essentials/ncr.cpp
@@ -0,0 +1,28 @@
+const int N=1e6+5;
+lli fac[N];
+void fact()
+{
+    fac[0]=1;
+    for(int i=1;i<N;i++)
+    {
+        fac[i]=(fac[i-1]*i)%mod;
+    }
+    return ;
+}
+lli ncr(lli a,lli b)
+{
+    return ((fac[a]*modi(fac[b],mod))%mod*modi(fac[a-b],mod))%mod;
+}
+int main()
+{
+    kira;
+    lli n,a,b;
+    cin >> n;
+    fact();
+    while(n--)
+    {
+        cin>>a>>b;
+        cout<<ncr(a,b)<<endl;
+    }
+    return 0;
+}
