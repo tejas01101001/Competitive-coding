5e8ff9346e3d1e10d5bd433e2738e6f3b0f09fd6
Tejas khairnar <tejas18@iitg.ac.in>
2020-06-18 02:44:59 +0530

    ncr_faster

--- Maths/ncr.cpp
+++ Maths/ncr.cpp
@@ -1,19 +1,21 @@
 const int N = 1e6 + 5;
-lli fac[N];
+lli fac[N], invfac[N];
 void fact()
 {
-    fac[0] = 1;
+    invfac[0]=fac[0] = 1;
     for (int i = 1; i < N; i++)
     {
         fac[i] = (fac[i - 1] * i) % mod;
+        invfac[i] = modi(fac[i]);
     }
     return;
 }
+
 lli ncr(lli a, lli b)
 {
     if (a < b || a < 0 || b < 0)
         return 0;
-    return ((fac[a] * modi(fac[b], mod)) % mod * modi(fac[a - b], mod)) % mod;
+    return ((fac[a] * invfac[b]) % mod * invfac[a - b]) % mod;
 }
 int main()
 {
