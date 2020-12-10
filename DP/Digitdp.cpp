2200413478b0519e65bcf29061faafd48f43f842
tejas01101001 <45798981+tejas01101001@users.noreply.github.com>
2020-12-10 15:25:35 +0530

    Update Digitdp.cpp

--- DP/Digitdp.cpp
+++ DP/Digitdp.cpp
@@ -1,72 +1,86 @@
-1)There are many types of problems that ask to count the number of integers ‘x‘ between two integers say ‘a‘ and ‘b‘ such that x satisfies a specific property that can be related to its digits.
+1)There are many types of problems that ask to count the number
+of integers ‘x‘ between two integers say ‘a‘ and ‘b‘ such that 
+x satisfies a specific property that can be related to its digits.
 
-2)G(x) tells the number of such integers between 1 to x (inclusively), then the number of such integers between a and b can be given by G(b) – G(a-1).
+2)G(x) tells the number of such integers between 1 to x 
+(inclusively), then the number of such integers between a and b 
+can be given by G(b) – G(a-1).
 
-3)tn tn-1 tn-2 … t2 t1 as the decimal representation where ti tells the i-th digit from the right. The leftmost digit tn is the most significant digit.
+3)tn tn-1 tn-2 … t2 t1 
+as the decimal representation where ti tells the i-th digit from 
+the right. The leftmost digit tn is the most significant digit.
 
-4)Now, after representing the given number this way we generate the numbers less than the given number and simultaneously calculate using DP, if the number satisfy the given property. We start generating integers having number of digits = 1 and then till number of digits = n. Integers having less number of digits than n can be analyzed by setting the leftmost digits to be zero.
+4)Now, after representing the given number this way we generate 
+the numbers less than the given number and simultaneously 
+calculate using DP, if the number satisfy the given property.
 
-DP STATE:
-lli solve(lli idx,G(x)/*parameters*/,lli tight,vector<lli>num)
+We start generating integers having number of digits = 1 and 
+then till number of digits = n. Integers having less number of 
+digits than n can be analyzed by setting the leftmost digits to
+be zero.
 
-NOTE:here num is x;
-intialise dp[A][B][2];
+5)Convert the given number to a string.
+say sn sn-1 .... s1.
 
-A:max(idx)+1
-B:max(G)+1
-C:2 (since tight is always 0 or 1)
+6)Declare the string globally to avoid useless recursive calls.
 
-1)idx:
-It tells about the index value from right in the given integer.
-And from left if vector<lli>num is reversed;
+7)Standard digit dp question.
 
-2)tight:
-This will tell if the current digits range is restricted or not. If the current digit’s.
-range is not restricted then it will span from 0 to 9 (inclusively) else it will span
-from 0 to num[idx] (inclusively).
+Lets call some positive integer classy if its decimal 
+representation contains no more than 3 non-zero digits. 
 
-i.e if(tight>0)limit=9;
-    else limit=num[idx];
+You are given a segment [𝐿;𝑅]. 
+Count the number of classy integers 𝑥 such that 𝐿≤𝑥≤𝑅.
 
-3)G:given parameters;
 
-lli solve(idx,G,tight,vector<lli>num)
+>N: upper limit in number of digits in number.
+>M: upper limit of Max value of function G(x)
+>2: tight specifies whether we are free to chose any number 
+    as the current digit.
+
+int dp[N][M][2];
+string s;
+
+int solve(int pos, int cnt, int t)
 {
- if(idx==num.si)
- {
-   ??
- }
- if(dp[idx][G][tight]!=-1)
-   return dp[idx][G][tight];
-STATE RELATION:
-//MEMOIZATION IS USED
+	if (pos == int(s.size()))
+	{
+		return cnt <= 3;
+	}
 
-lli limit=(tight?9:num[idx]);
-lli ans=0;
-for(int i=0;i<=limit;i++)
-{
-  //CONDITION due to G(x) make currG;
+	if (dp[pos][cnt][t] != -1)
+		return dp[pos][cnt][t];
 
-  lli currtight=tight;
-  if(i<num[idx])currtight=1;
+	int lim = t ? s[pos] - '0' : 9;
+	int ans = 0;
 
-  ans+=solve(idx+1,currG,c,num)
-  //idx+1 if from left idx else idx-1
-}
-return dp[idx][G][tight]=ans;
-}
+	for (int i = 0; i <= lim; i++)
+	{
+		ans += solve(pos + 1, cnt + (i != 0), t & (i == lim));
+	}
 
-lli count(lli x)
-{
-  vector<lli>num;
-  while(x)
-  {
-    num.pb(x%10);
-    x/=10;
-  }
-  //reverse(all(num));
-  memset(dp,-1,sizeof(dp));
-  return solve(0,0,0,num);
+	return dp[pos][cnt][t] = ans;
 }
 
-cout<<count(r)-count(l-1)<<endl;
+int main()
+{
+	cin >> a >> b;
+
+	int i = a.size() - 1;
+	while (a[i] == '0')
+	{
+		a[i] == '9';
+		i--;
+	}
+	a[i]--;
+
+	s = a;
+	memset(dp, -1, sizeof(dp));
+	int l = solve(0, 0, 1);
+
+	s = b;
+	memset(dp, -1, sizeof(dp));
+	int r = solve(0, 0, 1);
+
+	cout << r - l;
+}
